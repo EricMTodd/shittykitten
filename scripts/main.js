ba2bca562b831fcc8f705cd58c0aa2d30d77b45e
EricMTodd <eterictodd@gmail.com>
2021-01-25 00:06:28 -0700

    Successfully looped over artwork, displaying it in the gallery.

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+// Loop to display gallery images
+let files = {'png': 35};
+for (let ext in files) {
+  for (let i = 0; i < files[ext]; i++) {
+    let src = `../images/markham-maes-art-${i+1}.${ext}`
+    // console.log(src);
+    let img = new Image();
+    img.src = src;
+    
+    document.getElementById("gallery-test").appendChild(img);
+  }
+}
