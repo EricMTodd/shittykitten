fd18e6265e3e9a441db6f2a4e194ffff57992653
Eric M. Todd <eterictodd@gmail.com>
2021-05-19 11:45:25 -0600

    Basic style implementation in place.

--- src/views/galleryViews.js
+++ src/views/galleryViews.js
@@ -1,9 +1,32 @@
 const galleryViews = (() => {
+	let main = document.querySelector('main');
+
+	const renderPreviousButton = (() => {
+		let button = document.createElement('button');
+		button.className = 'gallery-controller-button';
+		button.id = 'previous-button';
+		button.innerText = '<';
+		button.addEventListener('click', (e) => {
+			console.log('click');
+		});
+		main.appendChild(button);
+	})();
+
 	const renderPortrait = (() => {
-		let main = document.querySelector('main');
 		let image = document.createElement('img');
 		image.src = '../dist/images/markham-self-portrait.png';
 		main.appendChild(image);
+	})();
+
+	const renderNextButton = (() => {
+		let button = document.createElement('button');
+		button.className = 'gallery-controller-button';
+		button.id = 'previous-button';
+		button.innerText = '>';
+		button.addEventListener('click', (e) => {
+			console.log('click');
+		});
+		main.appendChild(button);
 	})();
 	return {};
 })();
