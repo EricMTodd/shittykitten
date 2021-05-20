ecfa2672d2505c4a0aaae573818896bbb013ffc2
Eric M. Todd <eterictodd@gmail.com>
2021-05-20 14:32:36 -0600

    Timed slideshow implemented.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import { galleryViews } from '../src/views/galleryViews';
 import { galleryControllers } from '../src/controllers/galleryControllers';
 
-const app = (() => {})();
+const app = (() => {
+	const advanceGallery = window.setInterval(() => {
+		galleryControllers.nextImage();
+	}, 6000);
+})();
